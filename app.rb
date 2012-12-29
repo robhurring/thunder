6fa3498c639ca068a113d40d5b1f34b00546b6b0
Rob <robhurring@gmail.com>
2012-12-29 10:53:52 -0500

    commenting and cleaning up some old code

--- app.rb
+++ app.rb
@@ -13,15 +13,6 @@ class App
   include_package 'java.awt.event'
   include Profligacy
 
-  class JIdentifiedButton < JButton
-    attr_reader :identifier
-
-    def initialize(name, identifier)
-      super(name)
-      @identifier = identifier
-    end
-  end
-
   class MoveAction < AbstractAction
     def initialize(delegate, start_or_stop, direction)
       super()
@@ -49,75 +40,101 @@ class App
   def initialize
     @launcher = Launcher.connect
     @buttons = {
-      up:     JIdentifiedButton.new('up', :up),
-      down:   JIdentifiedButton.new('down', :down),
-      left:   JIdentifiedButton.new('left', :right),
-      right:  JIdentifiedButton.new('right', :left),
+      up:     JButton.new('up'),
+      down:   JButton.new('down'),
+      left:   JButton.new('left'),
+      right:  JButton.new('right'),
       fire:   JButton.new('fire!')
     }
 
     setup_ui
   end
 
+  # Public: Move the launcher
+  #
+  # start_or_stop - Send :start or :stop to start or stop moving
+  # direction     - Send :up, :down, :left, :right for the direction
+  #
+  # Examples
+  #
+  #   move(:start, :right)
+  #
   def move(start_or_stop, direction)
     case start_or_stop.to_sym
     when :start, :mousePressed
+      # Passing a non negative instead of -1 will move for that number of seconds
       @launcher.move(direction, -1)
     when :stop, :mouseReleased
       @launcher.stop!
     end
   end
 
+  # Public: Fire the launcher
   def fire
     @launcher.fire!
   end
 
 private
 
+  # Public: Build our swing UI using LEL (LEL is awesome)
+  # See: http://ihate.rubyforge.org/profligacy/lel.html
+  #
+  # TODO: breakup into smaller chunks or abstract out into different class
   def setup_ui
     app_layout = %{
       [ (200,200)^direction_pad ]
-      [ (50,100)>fire_button  ]
+      [ (50,100)>fire_button    ]
     }
 
+    # FIXME: LEL parser doesn't like this spaced out for some reason
     direction_pad_layout = %{
       [_|up|_]
       [left|_|right]
       [_|down|_]
     }
 
+    # Build directional pad group layout
     direction_pad = Swing::LEL.new(JPanel, direction_pad_layout) do |c, i|
+      # Inject our buttons into the layout
       c.up    = @buttons[:up]
       c.down  = @buttons[:down]
       c.left  = @buttons[:left]
       c.right = @buttons[:right]
 
+      # Inject our button actions
+      # TODO: hook these into existing MoveAction
       i.up    = {mouse: proc{ |t, e| move(t, :up) } }
       i.down  = {mouse: proc{ |t, e| move(t, :down) } }
       i.left  = {mouse: proc{ |t, e| move(t, :left) } }
       i.right = {mouse: proc{ |t, e| move(t, :right) } }
     end.build
 
+    # Build main layout
     main_ui = Swing::LEL.new(JFrame, app_layout) do |c, i|
       c.direction_pad = direction_pad
       c.fire_button = @buttons[:fire]
 
       i.fire_button = {action: proc{ fire } }
-    end.build(:args => "Simple LEL Example")
+    end.build(args: 'Fire Ze Missiles!!')
 
-    root = main_ui.getRootPane
+    root      = main_ui.getRootPane
+    inputMap  = root.getInputMap(JComponent::WHEN_ANCESTOR_OF_FOCUSED_COMPONENT)
+    actionMap = root.getActionMap
 
+    # Bind our arrow keys for movement
     [:up, :down, :left, :right].each do |direction|
-      root.getInputMap(JComponent::WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(KeyStroke.getKeyStroke(direction.to_s.upcase), "key-#{direction}")
-      root.getActionMap.put("key-#{direction}", MoveAction.new(self, :start, direction))
+      inputMap.put(KeyStroke.getKeyStroke(direction.to_s.upcase), "key-#{direction}")
+      inputMap.put(KeyStroke.getKeyStroke("released #{direction.to_s.upcase}"), "key-#{direction}-released")
 
-      root.getInputMap(JComponent::WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(KeyStroke.getKeyStroke("released #{direction.to_s.upcase}"), "key-#{direction}-released")
-      root.getActionMap.put("key-#{direction}-released", MoveAction.new(self, :stop, direction))
+      actionMap.put("key-#{direction}", MoveAction.new(self, :start, direction))
+      actionMap.put("key-#{direction}-released", MoveAction.new(self, :stop, direction))
     end
 
-    root.getInputMap(JComponent::WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).put(KeyStroke.getKeyStroke("ENTER"), "key-fire")
-    root.getActionMap.put("key-fire", FireAction.new(self))
+    # Bind ENTER to the fire action
+    inputMap.put(KeyStroke.getKeyStroke("ENTER"), "key-fire")
+    actionMap.put("key-fire", FireAction.new(self))
   end
 end
 
+# Boot it up
 SwingUtilities.invoke_later proc { App.new }.to_runnable
--- lib/launcher.rb
+++ lib/launcher.rb
@@ -1,3 +1,11 @@
+# Public: Controller for the thunder USB missile launcher
+#
+# Examples
+#
+#   l = Launcher.connect
+#   l.move(:right, 1)     # move right 1 second
+#   l.fire!(2)            # fire 2 missiles
+#
 class Launcher
   DeviceNotFoundError = Class.new(IOError)
 
@@ -23,8 +31,10 @@ class Launcher
   REQUEST_TYPE  = 0x21
   REQUEST       = 0x09
   PAYLOAD_START = 0x02
-  MAX_DURATION  = 5 #seconds
 
+  # Public: Scan the connected USB devices for the launcher
+  #
+  # Returns a new Launcher instance if found, otherwise raises DeviceNotFoundError
   def self.connect
     usb = LIBUSB::Context.new
     launcher = usb.devices(
@@ -41,6 +51,20 @@ class Launcher
     @handle = @device.open
   end
 
+  # Public: Move the launcher.
+  #
+  # direction - The direction to move, should be :left, :right, :up or :down
+  # duration  - How long to move for in seconds. Use -1 to keep moving until
+  #             a #stop! command is manually issued
+  #
+  # Examples
+  #
+  #   move(:right, 1)   # move right for 1 second
+  #
+  #   move(:right, -1)  # start moving right
+  #   sleep(1)          # pause for 1 second
+  #   stop!             # stop moving
+  #
   def move(direction, duration = 0.5)
     send! direction
 
@@ -50,16 +74,34 @@ class Launcher
     end
   end
 
+  # Public: Wrappers around the #move method
+  #
+  # duration - How long to move for in seconds
+  #
+  # Examples
+  #
+  #   right(1)    # moves right for 1 second
+  #
+  #   right!      # move right until manually stopped
+  #
   [:up, :down, :left, :right].each do |direction|
     define_method direction do |duration=0.5|
       move direction, duration
     end
 
     define_method :"#{direction}!" do
-      move direction, MAX_DURATION
+      move direction, -1
     end
   end
 
+  # Public: Fire (n) missiles
+  #
+  # number - The number of missiles to fire (between 1 and 4)
+  #
+  # Examples
+  #
+  #   fire!(2)  # fire 2 missiles
+  #
   def fire!(number = 1)
     number = 1 unless (1..MISSILES[:number]).cover?(number)
 
@@ -69,23 +111,41 @@ class Launcher
     end
   end
 
+  # Public: Stops all movement to the Launcher
   def stop!
     send! :stop
   end
 
 private
 
+  # Private: Send a command to the launcher
+  #
+  # command - The command to send to the launcher
+  #
+  # Examples
+  #
+  #   send!(0x20)   # send the STOP command to the launcher
+  #
   def send!(command)
+    payload = build_payload(COMMANDS[command.to_sym])
+
     @handle.control_transfer(
       bmRequestType: REQUEST_TYPE,
       bRequest: REQUEST,
       wValue: 0,
       wIndex: 0,
-      dataOut: build_payload(COMMANDS[command.to_sym]),
+      dataOut: payload,
       timeout: 0
     )
   end
 
+  # Private: Build a USB formatted packet to send to the launcher
+  #
+  # Examples
+  #
+  #   build_payload(0x10)
+  #
+  # Returns the byte packet to send to the launcher
   def build_payload(command)
     [PAYLOAD_START, command, 0, 0, 0, 0, 0, 0].pack('CCCCCCCC')
   end
